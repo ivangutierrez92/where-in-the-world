1a251bd29b65971bee4ad1d8ce69c0f2b2ee51a6
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-04-13 14:50:11 -0400

    added path to image loader

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: 'akamai',
+    path: '/where-in-the-world',
     domains: ['upload.wikimedia.org', 'flagcdn.com'],
   },
   productionBrowserSourceMaps: true,
